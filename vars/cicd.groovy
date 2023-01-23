b84d931d843e038843e454cb6e110b04244cc90e
thej950 <121796755+thej950@users.noreply.github.com>
2023-01-23 14:51:36 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,20 @@
+def newGit(repo)
+{
+    git "${repo}"
+}
+def newMaven()
+{
+    sh 'mvn package'
+}
+def newDeploy(workspace,ip,context)
+{
+    sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+def runSelenium(workspace)
+{
+    sh "java -jar /var/lib/jenkins/workspace/${workspace}/testing.jar"
+}
+def newDelivery(workspace,ip,context)
+{
+    sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
