3a84ff35a9d3560101766c9f91ff4454af8927e8
thej950 <121796755+thej950@users.noreply.github.com>
2023-10-05 12:36:56 +0530

    Update cicd1.groovy

--- vars/cicd1.groovy
+++ vars/cicd1.groovy
@@ -2,15 +2,3 @@ def gitDownload(repo)
 {
   git "https://github.com/thej950/${repo}.git"
 }
-def build()
-{
-  sh "mvn package"
-}
-def Deploy("jobname","ip","appname")
-{
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
-}
-def runselenium()
-{
-sh " java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
-}
