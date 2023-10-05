16492e05e68362c6c18c235f588182af12039da4
thej950 <121796755+thej950@users.noreply.github.com>
2023-10-05 12:47:10 +0530

    Update cicd1.groovy

--- vars/cicd1.groovy
+++ vars/cicd1.groovy
@@ -6,3 +6,7 @@ def build()
 {
   sh "mvn package"
 }
+def DeployQA("jobname","ip","appname")
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
