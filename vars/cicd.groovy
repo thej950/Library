f45fa6f0b0c4445f80e65d146a3fe45ef88e65d4
thej950 <121796755+thej950@users.noreply.github.com>
2023-10-18 17:37:40 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,7 +8,7 @@ def newMaven()
 }
 def newDeploy(workspace,ip,context)
 {
-    sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+    sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war vagrant@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
 def runSelenium(workspace)
 {
