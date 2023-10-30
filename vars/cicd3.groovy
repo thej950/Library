5c25eba273e74d9aca5db903b26e269068018a89
thej950 <121796755+thej950@users.noreply.github.com>
2023-10-30 15:44:08 +0530

    Create cicd3.groovy

---
+++ vars/cicd3.groovy
@@ -0,0 +1,4 @@
+def newDeploy(workspace,username,ip,context)
+{
+    sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war {username}@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
